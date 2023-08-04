d2f4cfe60eca369d022151cd046121d36e04e320
Alm1ra <frydlewicz.ola@gmail.com>
2023-08-04 23:54:43 +0200

    Fixed choosing letters feature

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const url = 'https://random-word-api.herokuapp.com/word?lang=en';
+let currWord = '';
 window.onload = function() {
     letterGrid();
-    //makeWord();
 }
 
 function letterGrid() {
@@ -11,6 +11,7 @@ function letterGrid() {
     for(i of alphabet) {
         let btn = document.createElement("button");
         btn.innerHTML = i;
+        btn.id = 'alph-btn'
         letters.appendChild(btn);
     }
 }
@@ -30,18 +31,46 @@ function makeWord(data) {
 async function getWord(url) {
     const response = await fetch(url);
     let data = await response.json();
-    console.log(data[0]);
-    makeWord(data[0]);
+    let word = data[0].toUpperCase();
+    currWord = word;
+    console.log(currWord);
+    makeWord(word);
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    const wordBtn = document.getElementById('reset');
+    let genWord = false;
+    const resetBtn = document.getElementById('reset');
+    const letters = document.getElementById('grid-container');
     
-    if(wordBtn) {
-        wordBtn.addEventListener("click", function() {
+    if(resetBtn) {
+        resetBtn.addEventListener('click', function() {
+            genWord = true;
+           const alphButtons = document.querySelectorAll('#alph-btn');
+
+            for(button of alphButtons) {
+                button.style.color = '#747474';
+            }
             getWord(url)
         });
     }
+
+    letters.addEventListener('click', e => {
+        const isButton = e.target.nodeName === 'BUTTON';
+        if(!isButton) {return;}
+
+        let char = e.target.textContent;
+        let color = e.target.style.color;
+
+        if(color !==  'rgb(255, 228, 0)' && genWord === true) {
+            e.target.style.color = "#FFE400";
+        }
+
+        if(currWord.includes(char) && color !==  'rgb(255, 228, 0)' && genWord === true) {
+                console.log("TRUEEEEEE");
+        }
+    })
+
+
 })
 
 
